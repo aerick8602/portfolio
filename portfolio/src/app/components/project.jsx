8c220535b079c358464a411ad34d9d665f69fca2
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-12-05 22:39:52 +0530

    res tagerror

--- portfolio/src/app/components/project.jsx
+++ portfolio/src/app/components/project.jsx
@@ -40,7 +40,7 @@ const Project = ({data,link}) => {
                   <a href={url} target="_blank" rel="noopener noreferrer" style={style.link}>
                     {link}
                   </a>
-                  <span> {"target=\"_blank\""}&gt;<span style={style.default}>Live Demo</span>&lt;Link&gt;</span>
+                  <span> target=&quot;{"_blank"}&quot;&gt;<span style={style.default}>Live Demo</span>&lt;Link&gt;</span>
                   <br />
                 </span>
               );
