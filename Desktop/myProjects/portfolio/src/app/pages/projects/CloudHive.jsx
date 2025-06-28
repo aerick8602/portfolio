21911ec776c6e38a41aabd79d9b5a2aa0d2745e1
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2025-06-28 21:59:05 +0530

    Delete Desktop/myProjects/portfolio directory

--- Desktop/myProjects/portfolio/src/app/pages/projects/CloudHive.jsx
+++
@@ -1,9 +0,0 @@
-import Project from "@/app/components/project";
-import ProjectDetails from "@/app/utils/projectlist";
-
-const CloudHive = () => {
-  const data = ProjectDetails.CloudHive;
-  const link = ProjectDetails.CloudHiveLink;
-  return <Project data={data} link={link}></Project>;
-};
-export default CloudHive;
--- Desktop/myProjects/portfolio/src/app/toolkit/filestore.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fileReducer from './fileslice'; 
-
-export const Filestore = configureStore({
-  reducer: {
-    files: fileReducer, 
-  },
-});
